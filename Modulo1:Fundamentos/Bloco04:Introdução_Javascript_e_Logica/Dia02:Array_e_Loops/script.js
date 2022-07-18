5003afe00370af3279d54b25984869446e463c78
Erick Argollo <argolloerickdev@gmail.com>
2022-07-18 12:13:43 -0300

    adiciona ex 04

--- Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia02:Array_e_Loops/script.js
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia02:Array_e_Loops/script.js
@@ -32,5 +32,27 @@ for (let i = 0; i < numbers.length; i++) {
 let mediaArit = resultado / numbers.length;
 
 console.log(mediaArit);
-*/
+
 //4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+
+    resultado = resultado + numbers[i];
+}
+
+let mediaArit = resultado / numbers.length;
+
+if(mediaArit > 20) {
+    console.log("Media > 20")
+}
+else {
+    console.log("Media <= 20")
+}
+
+*/
+
+//5
