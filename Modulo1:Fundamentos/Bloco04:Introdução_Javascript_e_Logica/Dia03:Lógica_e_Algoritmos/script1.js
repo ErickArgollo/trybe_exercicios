51dc719f6ac9118e36d0bbcc9905d4624a452cef
Erick Argollo <argolloerickdev@gmail.com>
2022-07-19 09:31:36 -0300

    adiciona ex2

--- Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia03:Lógica_e_Algoritmos/script1.js
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia03:Lógica_e_Algoritmos/script1.js
@@ -4,3 +4,13 @@ for (var i=1; i <= 100; i ++){
     sum += i
 }
 console.log(sum)
+
+
+let value = 100;
+let contador = 0;
+while (value > 0){
+    value -= 5
+    contador += 1
+    console.log(`Foi executado ${contador} vez`)
+}
+console.log(value)
