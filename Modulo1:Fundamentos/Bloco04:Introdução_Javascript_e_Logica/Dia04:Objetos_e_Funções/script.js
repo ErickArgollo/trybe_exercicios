5e575118c562a09c32db742b7986e1384c85a7ef
Erick Argollo <argolloerickdev@gmail.com>
2022-07-22 09:49:46 -0300

    finalização dos exercicios de fixação

--- Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia04:Objetos_e_Funções/script.js
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia04:Objetos_e_Funções/script.js
@@ -1,3 +1,4 @@
+/*
 //teste para fixar
 let pessoa = {
     nome: 'Erick',
@@ -74,10 +75,10 @@ console.log(Pessoas[0]);
 console.log(primeiraPessoa['nome'])
 
 // Exéricio 1 para fixar 
-/*let name = 'Marta';
+let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
-let medals = { golden: 2, silver: 3 };*/
+let medals = { golden: 2, silver: 3 };
 
 let player = {
     name: 'Marta',
@@ -95,3 +96,42 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 
 console.log(`A jogadora ${player['name']} foi eleita a melhor do mundo por ${player['bestInTheWorld'].length} vezes`)
 console.log(`A jogadora possui ${player['medals']['golden']} medalhas de ouro e ${player.medals.silver} de prata`)
+
+// Exercicio 2 para fixar (for/in)
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for(let i in names){
+    console.log(`Olá ${names[i]}`)
+  }
+ // Exercicio 3 para fixar
+ let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  }; 
+
+  for(let i in car){
+    console.log(`${i}: ${car[i]}`)
+  }
+*/
+
+ function name(string1){
+    return string1;
+ }
+
+ function lastName(string){
+    return string;
+ }
+
+ function saudacao(){
+    return `Olá, ${name('Erick')} ${lastName('Argollo')}!`;
+ }
+
+ saudacao();
+
+ 
