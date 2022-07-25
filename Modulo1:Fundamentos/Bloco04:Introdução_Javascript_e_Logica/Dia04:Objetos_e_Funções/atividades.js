3e6c00304e6c30e28f2b66a0a620318300bc696f
Erick Argollo <argolloerickdev@gmail.com>
2022-07-25 12:11:13 -0300

    adiciona ex 5

--- Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia04:Objetos_e_Funções/atividades.js
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia04:Objetos_e_Funções/atividades.js
@@ -326,7 +326,41 @@ let ordem = basket.sort();
 console.log(`O morador do bloco 2 de nome ${moradores['blocoDois'][1]['nome']} mora no ${moradores['blocoDois'][1]['andar']} andar, apartamento ${moradores['blocoDois'][1]['apartamento']}`)
 */
 //bonus 5
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
 
+//Bloco 1
+for(let i = 0; i < moradores['blocoUm'].length; i += 1){
+  console.log(moradores['blocoUm'][i]['nome'] + ' ' + moradores['blocoUm'][i]['sobrenome'])
+}
 
 
 
