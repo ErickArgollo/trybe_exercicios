1799ae1de062a30bde545c782911f010c6bb926e
Erick Argollo <argolloerickdev@gmail.com>
2022-08-10 15:16:13 -0300

    feat(exercicios): exercicio 4 parte 2 resolvido de 2 formas diferentes

--- Modulo1:Fundamentos/Bloco07:JavaScript_ES6/script2.js
+++ Modulo1:Fundamentos/Bloco07:JavaScript_ES6/script2.js
@@ -29,20 +29,45 @@
 //2
 
 
-let string = 'Antônio foi no banheiro e não sabemos o que aconteceu';
+// let string = 'Antônio foi no banheiro e não sabemos o que aconteceu';
 
-console.log(string.split(' ').sort())
-const retornaMaior = (string) => {
+// console.log(string.split(' ').sort())
+// const retornaMaior = (string) => {
 
-    let arr = string.split(' ');
-    let maiorPalavra = arr[0];
-    for(let i = 0; i < arr.length; i += 1){
-        if(maiorPalavra.length < arr[i].length){
-            maiorPalavra = arr[i];
-        }
+//     let arr = string.split(' ');
+//     let maiorPalavra = arr[0];
+//     for(let i = 0; i < arr.length; i += 1){
+//         if(maiorPalavra.length < arr[i].length){
+//             maiorPalavra = arr[i];
+//         }
         
+//     }
+//     return maiorPalavra;
+// }
+
+// console.log(retornaMaior('Antônio foi no banheiro e não sabemos o que aconteceu'))
+
+//4 forma como a questão pediu
+
+// const substituaX = (nome) => `Tryber ${nome} aqui!`;
+
+// console.log(substituaX('Erick'));
+
+//4 Forma 2
+
+const teste = 'ola eu sou erick';
+
+const arr = teste.split('')
+
+const substituaX = (frase) => {
+    const arr = frase.split('');
+
+    for(let i = 0; i <  arr.length; i+=1){
+        if(arr[i] == 'x'){
+            arr[i] = 'Trybe'
+        }
     }
-    return maiorPalavra;
+    return arr.join('')
 }
 
-console.log(retornaMaior('Antônio foi no banheiro e não sabemos o que aconteceu'))
+console.log(substituaX('ola pessoax tux bem x com vocxs'))
