732d93f580d5f09265bd56d28f06bae6d747a603
Erick Argollo <argolloerickdev@gmail.com>
2022-08-10 15:04:57 -0300

    feat(exercicios): exercicio 2 parte 2

--- Modulo1:Fundamentos/Bloco07:JavaScript_ES6/script2.js
+++ Modulo1:Fundamentos/Bloco07:JavaScript_ES6/script2.js
@@ -26,5 +26,23 @@
 
 // console.log(factorial(5))
 
+//2
 
 
+let string = 'Antônio foi no banheiro e não sabemos o que aconteceu';
+
+console.log(string.split(' ').sort())
+const retornaMaior = (string) => {
+
+    let arr = string.split(' ');
+    let maiorPalavra = arr[0];
+    for(let i = 0; i < arr.length; i += 1){
+        if(maiorPalavra.length < arr[i].length){
+            maiorPalavra = arr[i];
+        }
+        
+    }
+    return maiorPalavra;
+}
+
+console.log(retornaMaior('Antônio foi no banheiro e não sabemos o que aconteceu'))
