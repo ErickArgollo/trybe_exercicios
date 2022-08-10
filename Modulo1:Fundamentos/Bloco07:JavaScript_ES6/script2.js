64aa1ca738d2f8466565072d7b81f7f235087bcc
Erick Argollo <argolloerickdev@gmail.com>
2022-08-10 14:14:51 -0300

    feat(exercicios): exercicio 1 parte 2

---
+++ Modulo1:Fundamentos/Bloco07:JavaScript_ES6/script2.js
@@ -0,0 +1,30 @@
+// const factorial = (number) => {
+    
+//     let result = 1;
+
+//     for(let i = 1; i <= number; i += 1){
+//         result *= i
+//     }
+
+//     return result;
+// }
+
+// console.log(factorial(4))
+
+//metodo recursivo
+
+// const factorial = (number) => {
+    
+//     let result = number;
+
+//     for(let i = number; i > 1; i -= 1){
+//         result *= i-1
+//     }
+
+//     return result;
+// }
+
+// console.log(factorial(5))
+
+
+
