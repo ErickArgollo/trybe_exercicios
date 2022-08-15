3627242acaa40e649792d87434dff7941402b778
Erick Argollo <argolloerickdev@gmail.com>
2022-08-15 10:57:18 -0300

    feat(exercicios): adiciona exercicios 1 2 de Jest

--- Modulo1:Fundamentos/Bloco07:JavaScript_ES6/Dia03:Testes_Unitários/sum.js
+++ Modulo1:Fundamentos/Bloco07:JavaScript_ES6/Dia03:Testes_Unitários/sum.js
@@ -1,4 +1,42 @@
-const sum = (a, b) => a + b;
+const sum = (a, b) =>{
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        throw new Error ('parameters must be numbers');
+    }
+    return a + b;
+} 
+
 const sub = (a, b) => a - b;
 
-module.exports = { sum, sub}
+const myRemove = (array, item) => {
+    let newArr = [];
+    for(let i = 0; i < array.length; i += 1){
+        if(array[i] !== item){
+            newArr.push(array[i])
+        }
+    }
+    return newArr;
+}
+
+const fizzBuzz = (num) => {
+    
+
+    if(typeof num !== 'number'){
+        return false;
+    }
+
+    if(num % 3 == 0 && num % 5 === 0){
+        return 'fizzBuzz';
+    }
+    else if(num % 3 === 0){
+        return 'buzz';
+    }
+    else if(num % 5 === 0){
+        return 'fizz';
+    }
+    
+        return num;
+}
+
+
+
+module.exports = { sum, sub, myRemove, fizzBuzz}
--- Modulo1:Fundamentos/Bloco07:JavaScript_ES6/Dia03:Testes_Unitários/sum.test.js
+++ Modulo1:Fundamentos/Bloco07:JavaScript_ES6/Dia03:Testes_Unitários/sum.test.js
@@ -1,10 +1,11 @@
 // const sum = require('./sum');
 // const sub = require('./sub')
 const {
-  sum, sub
-} = require('./sum')
+  sum, sub, myRemove, fizzBuzz
+} = require('./sum');
 
-describe('sum', () =>{ 
+describe('testes sum', () =>{ 
+
 it('sums two values', () => {
   expect(sum(2, 3)).toEqual(5);
 });
@@ -12,11 +13,39 @@ it('sums two values', () => {
 it('testa se 1 e 2 é igual a 3', () => {
   expect(sum(1, 2)).toBe(3);
 })
+
+it('Testa se a função sum lança um erro quando os parâmetros são 4 e "5" string 5' , () => {
+  expect(()=>sum(4, '5')).toThrow('parameters must be numbers')
+})
+
 }),
 
 
-describe('sub', ()=>{
+describe('testes sub', ()=>{
   it('Subtração de 2 e 3', ()=>{
     expect(sub(3, 2)).toEqual(1)
+  })
+})
+
+
+describe('Testes myRemove', () => {
+
+  it('Verifica se ao receber o array [1,2,3,4] e 3, retorna o array [1,2,4]', () =>{
+    expect(myRemove([1,2,3,4], 3)).toEqual([1,2,4])
+  })
+
+  it('Verifica se ao receber o array [1,2,3,4] e 3, não retorna o array [1,2,3,4]', () =>{
+    expect(myRemove([1,2,3,4], 3)).not.toEqual([1,2,3,4])
+  })
+
+  it('Verifica se ao receber o array [1,2,3,4] e 5, retorna o array [1,2,3,4]', () =>{
+    expect(myRemove([1,2,3,4], 5)).toEqual([1,2,3,4])
+  })
+
+})
+
+describe('testes do fizzbuzz', ()=>{
+  it('Verifica se ao chamar a função com o número 15, retorna fizzBuzz', () =>{
+    expect(fizzBuzz(15)).toBe('fizzBuzz')
   })
 })
